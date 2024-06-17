67a9b1dd37f9c7d9e507a74596028ad1ad6d1c62
rabi-nayg-gergo <rabi.nagy.gergo@gmail.com>
2024-06-17 17:33:54 +0200

    Fix: resposiveness

--- js/styleswitch.js
+++ js/styleswitch.js
@@ -2,6 +2,8 @@
 // http://ejohn.org/blog/flexible-javascript-events/
 // https://codepen.io/SitePoint/pen/GpzrPG
 
+const nav = document.getElementById("nav");
+
 function addEvent(obj, type, fn) {
   if (obj.attachEvent) {
     obj['e' + type + fn] = fn;
@@ -17,6 +19,15 @@ function switchStyles() {
     className = selectedOption.value;
 
   document.body.className = className;
+  if(className == "large")
+    {
+      nav.classList.remove("sticky-top");
+    }
+    else
+    {
+      nav.classList.add("sticky-top");
+    }
+  
 }
 
 var styleSwitcher = document.getElementById("styleSwitcher");
